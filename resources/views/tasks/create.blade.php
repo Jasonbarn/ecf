03075d5dd6430d141a5b71d9927ffb11ed496fe0
jason <barnajason27@gmail.com>
2024-08-13 11:30:46 +0100

    fin mise a jour

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,38 +1,6 @@
 @extends('layouts.task')
 
 @section('content')
-<h1>Création d'une nouvelle tâche</h1>
-
-<form action="{{ route('tasks.store') }}" method="POST">
-    @csrf 
-    <div>
-        <label for="title">Titre :</label>
-        <input type="text" name="title" id="title" value="{{ old('title') }}">
-        @error('title')
-            <div>{{ $message }}</div> 
-        @enderror
-    </div>
-    <div>
-        <label for="description">Description :</label>
-        <textarea name="description" id="description">{{ old('description') }}</textarea>
-    </div>
-        <button type="submit" class="bg-gradient-to-r from-red-400 to-yellow-500 text-white font-bold py-2 px-4 rounded">
-            Enregistrer
-        </button>
-</form>
-
-<br>
-<a href="{{ route('tasks.accueil') }}">
-    <button class="bg-gradient-to-r from-blue-400 to-indigo-500 text-white font-bold py-2 px-4 rounded">
-        Retour à la liste des tâches
-    </button>
-</a>
-@endsection
-
-
-@extends('layouts.task')
-
-@section('content')
 <body style="background-color:   rgb(218, 147, 109);">
     
     <div class="container mx-auto p-6">
@@ -68,4 +36,3 @@
 </a>
 </a>
 @endsection
-
